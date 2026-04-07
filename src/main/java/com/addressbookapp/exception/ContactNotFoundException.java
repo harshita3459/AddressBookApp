70140d48e3c9828722ed0262a6d96904e298fbb8
Harshita Agrawal <harshiagrawal3459@gmail.com>
2026-04-07 11:58:03 +0530

    [Harshita Agrawal] add. AddressBook app

---
+++ src/main/java/com/addressbookapp/exception/ContactNotFoundException.java
@@ -0,0 +1,9 @@
+package com.addressbookapp.exception;
+
+public class ContactNotFoundException extends RuntimeException {
+
+	    public ContactNotFoundException(String message) {
+	        super(message);
+	    }
+}
+
